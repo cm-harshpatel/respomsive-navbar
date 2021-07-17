647593a1d2602f397b808ffaa218a673227516f3
cm-harshpatel <harshp062@gmail.com>
2021-07-17 12:18:09 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const menuItems = document.querySelector(".menu-items");
+
+hamburger.addEventListener('click', ()=>{
+    console.log("clicked")
+})
